12befc3a38374cc008faf60a9ed8a7307688d9ad
lenastoj <jelenastojkecic@gmail.com>
2023-03-01 19:15:09 +0100

    prvi zadatak

---
+++ src/AnimalList.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AnimalList() {
+  const animals = [
+    {type: 'bird', name: 'duck', dateOfBirth: new Date().getFullYear() },
+    {type: 'bird', name: 'pigeon', dateOfBirth: new Date().getFullYear() },
+    {type: 'mammal', name: 'cat', dateOfBirth: new Date().getFullYear() },
+    {type: 'mammal', name: 'dog', dateOfBirth: new Date().getFullYear() },
+    {type: 'mammal', name: 'rabbit', dateOfBirth: new Date().getFullYear() }
+  ];
+
+  return(
+    <div className="App">
+      <h1>Animal List:</h1>
+      <table>
+        <thead>
+            <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Date Of Birth</th>
+            </tr>
+        </thead>
+        <tbody>
+            {animals.map((animal, key) => {
+            return (
+                <tr key={key}>
+                <td>{animal.type}</td>
+                <td>{animal.name}</td>
+                <td>{animal.dateOfBirth }</td>
+                </tr>
+            )
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default AnimalList;
+
--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import logo from './logo.svg';
+import AnimalList from './AnimalList';
 import './App.css';
 
-function AnimalList() {
-  const animals = [
-    {type: 'bird', name: 'duck', dateOfBirth: newDate() },
-    {type: 'bird', name: 'seaguell', dateOfBirth: newDate() },
-    {type: 'mammal', name: 'cat', dateOfBirth: newDate() },
-    {type: 'mammal', name: 'dog', dateOfBirth: newDate() },
-    {type: 'bird', name: 'mouce', dateOfBirth: newDate() },
-  ];
+function App() {
+  return (
+        <div className="App">
+          <AnimalList />
+        </div>
 
+  );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+export default App;
 
-// export default App;
